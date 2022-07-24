f95fea5d0d3330baac1133fa1cc705f40c85c17a
Maycon <mayyconaquino@gmail.com>
2022-07-24 16:10:06 -0300

    Criando classe herança Pessoa

--- src/main/java/com/barbearia/model/Cliente.java
+++ src/main/java/com/barbearia/model/Cliente.java
@@ -1,18 +1,13 @@
 package com.barbearia.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
-public class Cliente {
+@PrimaryKeyJoinColumn(name="idPessoa")
+public class Cliente extends Pessoa {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String nome;
+	private static final long serialVersionUID = 1L;
 	private String endereco;
 	private String telefone;
 	private String email;
@@ -24,35 +19,11 @@ public class Cliente {
 
 
 	public Cliente(String nome, String endereco, String telefone, String email) {
-		super();
-		this.nome = nome;
+		super(nome);
 		this.endereco = endereco;
 		this.telefone = telefone;
 		this.email = email;
 	}
-	
-	
-
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public String getNome() {
-		return nome;
-	}
-
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 
 	public String getEndereco() {
 		return endereco;
---
+++ src/main/java/com/barbearia/model/Pessoa.java
@@ -0,0 +1,45 @@
+package com.barbearia.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Pessoa implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idPessoa;
+	private String nome;
+
+	public Pessoa() {
+	}
+	
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(Long idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
